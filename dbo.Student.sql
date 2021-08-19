919d834e030d8fcb3085fb63da53161aba605673
MahasenEkanayake <75877892+MahasenEkanayake@users.noreply.github.com>
2021-08-19 18:54:17 +0530

    Add project files.

---
+++ dbo.Student.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Student] (
+    [ID]             INT            IDENTITY (1, 1) NOT NULL,
+    [LastName]       NVARCHAR (MAX) NULL,
+    [FirstMidName]   NVARCHAR (MAX) NULL,
+    [EnrollmentDate] DATETIME2 (7)  NOT NULL,
+    CONSTRAINT [PK_Student] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
